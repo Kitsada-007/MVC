7be7888bb2881c732b43d57b00415631ecc85ed8
Kitsada-007 <uroroom123@gmail.com>
2025-02-17 14:02:10 +0700

    NewMVC

--- databases/students.php
+++ databases/students.php
@@ -18,3 +18,51 @@ function getStudentsByKeyword(string $keyword): mysqli_result|bool
     $result = $stmt->get_result();
     return $result;
 }
+
+function deleteStudentsById(int $id): bool
+{
+    $conn = getConnection();
+    $sql = 'delete from students where student_id = ?';
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $id);
+    try {
+        $stmt->execute();
+        if ($stmt->affected_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (Exception $e) {
+        return false;
+    }
+}
+function getStudentById(int $id): mysqli_result|bool
+{
+    $conn = getConnection();
+    $sql = 'select * from students where student_id = ?';
+    $stmt = $conn->prepare($sql);    
+    $stmt->bind_param('i', $id);
+    $stmt->execute();    
+    $result = $stmt->get_result();
+    return $result;
+}
+
+function changePassword(int $id, string $password): bool
+{
+    $conn = getConnection();
+    $sql = 'update students set password = ? where student_id = ?';
+    $stmt = $conn->prepare($sql);
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $stmt->bind_param('si', $hash, $id);
+    try {
+        $stmt->execute();
+        if ($stmt->affected_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (Exception $e) {
+        return false;
+    }
+    return $result;
+}
